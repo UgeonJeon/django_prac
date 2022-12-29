a29964ebf98c3d8a2981eb668cd1ee2c8143cd36
UgeonJeon <ugeon.jeon@endoai.co.kr>
2022-12-29 16:55:35 +0900

    Add : "Django Project Setting"

---
+++ my_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+      'default' : {
+          'ENGINE': 'django.db.backends.mysql',
+          'NAME': 'ugeon',
+          'USER': 'root',
+          'PASSWORD': '',
+          'HOST': '127.0.0.1',
+          'PORT': '3306 ',
+      }
+}
+
+
+SECRET_KEY = 'django-insecure-aj=t92cxn&nn_vm_1nilzkg4r1xb4r@rwb)@dlvabz--zvj3k('
